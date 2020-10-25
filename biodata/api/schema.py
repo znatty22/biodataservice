a47593cb3a9c9fbf3adba3f0507ccc4a6a5bb749
Natasha Singh <dukedesi22@gmail.com>
2020-10-25 19:00:04 -0400

    :recycle: Split out object types, queries, mutations into own modules

--- biodata/api/schema.py
+++ biodata/api/schema.py
@@ -2,59 +2,24 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from biodata.api import models as m
+from biodata.api import queries
+from biodata.api import mutations
 
 
-class StudyType(DjangoObjectType):
-    class Meta:
-        model = m.Study
-        fields = ("kf_id", "name", "short_name")
+class Query(
+    queries.Query,
+    graphene.ObjectType
+):
+    pass
 
 
-class ParticipantType(DjangoObjectType):
-    class Meta:
-        model = m.Participant
-        fields = (
-            "kf_id", "gender", "race", "ethnicity", "biospecimens", "study"
-        )
+class Mutation(
+#    mt.CreateStudyMutation,
+    mutations.Mutation,
+#    mt.CreateBiospecimenMutation,
+    graphene.ObjectType
+):
+    pass
 
 
-class BiospecimenType(DjangoObjectType):
-    class Meta:
-        model = m.Biospecimen
-        fields = (
-            "kf_id", "analyte_type", "participant"
-        )
-
-
-class Query(graphene.ObjectType):
-    all_studies = graphene.List(StudyType)
-    all_participants = graphene.List(
-        ParticipantType, study_id=graphene.String(required=False)
-    )
-    all_biospecimens = graphene.List(
-        BiospecimenType, study_id=graphene.String(required=False)
-    )
-
-    def resolve_all_studies(root, info):
-        return m.Study.objects.all()
-
-    def resolve_all_participants(root, info, study_id=None):
-        objects = m.Participant.objects
-        if study_id:
-            objects = objects.filter(study=study_id)
-
-        return (
-            objects.select_related('study').all()
-        )
-
-    def resolve_all_biospecimens(root, info, study_id=None):
-        objects = m.Biospecimen.objects
-        if study_id:
-            objects = objects.filter(study=study_id)
-
-        return (
-            objects.select_related('participant').all()
-        )
-
-
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
